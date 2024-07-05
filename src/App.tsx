5e674193e955fab50d6f1deb168b771b1b351a0e
Maria Herne <noelia.herne@gmail.com>
2024-07-05 12:47:25 -0500

    Install and Configure Tailwind CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 function App() {
   return (
-    <div>
-      <p>Hello, world!</p>
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <div className="text-3xl font-bold underline">Hello, world!</div>
     </div>
   );
 }
 
 export default App;
+
